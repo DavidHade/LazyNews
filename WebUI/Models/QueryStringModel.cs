629906ee6a291fe799b899610e5cf9feeb93f90a
Deividas Vaikasas <10329396@mydbs.ie>
2019-12-15 17:50:15 +0200

    Side bar fixes

---
+++ WebUI/Models/QueryStringModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class QueryStringModel
+    {
+        public QueryStringModel()
+        {
+            TheGuardianString = "The Guardian";
+            TheGuardianQueryString = "theguardian";
+        }
+        //foxnews
+        //bbc
+        //nytimes
+        //theverge
+
+        public string TheGuardianString { get; set; } = "The Guardian";
+        public string TheGuardianQueryString { get; set; } = "theguardian";
+        public string FoxnewsString { get; set; } = "Fox News";
+        public string FoxnewsQueryString { get; set; } = "foxnews";
+        public string BbcString { get; set; } = "Bbc";
+        public string BbcQueryString { get; set; } = "bbc";
+        public string NytimesString { get; set; } = "New York Times";
+        public string NytimesQueryString { get; set; } = "nytimes";
+        public string ThevergeString { get; set; } = "The Verge";
+        public string ThevergeQueryString { get; set; } = "theverge";
+        
+
+    }
+}
