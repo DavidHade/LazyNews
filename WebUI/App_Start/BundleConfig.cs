bcead743072249cbcf38613dbcaa6c6783513647
David Vaikasas <david.vaikasas@gmail.com>
2020-01-02 21:10:07 +0000

    UI updades

--- WebUI/App_Start/BundleConfig.cs
+++ WebUI/App_Start/BundleConfig.cs
@@ -14,6 +14,14 @@ namespace WebUI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            // Bundling custom scripts
+            bundles.Add(new ScriptBundle("~/bundles/lazyloading").Include(
+                        "~/Scripts/lazyloading*"));
+            bundles.Add(new ScriptBundle("~/bundles/modal").Include(
+                        "~/Scripts/modal*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquerytimeago").Include(
+                        "~/Scripts/jquery.timeago*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,7 +32,8 @@ namespace WebUI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css").Include("~/Content/all.css", new CssRewriteUrlTransform()));
+
         }
     }
 }
